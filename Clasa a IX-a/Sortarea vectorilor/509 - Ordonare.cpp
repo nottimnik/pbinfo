e930318669835d470dde9c35def1f4f0d49c2752
Timnik <72356939+Timnikk@users.noreply.github.com>
2020-12-25 13:26:53 +0200

    Create 509 - Ordonare.cpp

---
+++ Clasa a IX-a/Sortarea vectorilor/509 - Ordonare.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, v[1001];
+    cin >> n;
+    for(int i = 1;i<=n;++i) {
+        cin >> v[i];
+    }
+    for(int i = 1;i<=n;++i) {
+        for(int j = i + 1;j<=n;++j) {
+            if(v[i]>v[j]) {
+                swap(v[i],v[j]);
+            }
+        }
+    }
+    for(int i = 1;i<=n;++i) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
