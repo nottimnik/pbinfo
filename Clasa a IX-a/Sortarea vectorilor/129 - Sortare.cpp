ac4e860cbe03a743598075a4dd791ad7ebffe748
Timnik <72356939+Timnikk@users.noreply.github.com>
2020-12-27 01:30:45 +0200

    Create 129 - Sortare.cpp

---
+++ Clasa a IX-a/Sortarea vectorilor/129 - Sortare.cpp
@@ -0,0 +1,25 @@
+#include<fstream>
+using namespace std;
+
+ifstream cin("sortare.in");
+ofstream cout("sortare.out");
+
+int main() {
+    int n, v[1001];
+    cin >> n;
+    for(int i = 1;i<=n;++i) {
+        cin >> v[i];
+    }
+    for(int i = 1;i<=n;++i) {
+        for(int j = i + 1;j<=n;++j) {
+            if(v[i]<v[j]) {
+                swap(v[i],v[j]);
+            }
+        }
+    }
+
+    for(int i = 1;i<=n;++i) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
