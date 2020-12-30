2d051f10884bee009b8f3eaef1c9910973453e86
Timnik <72356939+Timnikk@users.noreply.github.com>
2020-12-30 13:17:16 +0200

    Update 519 - Secventa1.cpp

--- Clasa a IX-a/Probleme cu secvente/519 - Secventa1.cpp
+++ Clasa a IX-a/Probleme cu secvente/519 - Secventa1.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int a[1000] , b[1000] , n , m , indice=0 ,ok=0;
+    
+    //declararea vectorilor
     cin >> n;
     for(int i = 0 ; i < n ; ++i)
     cin >> a[i];
     cin >> m;
     for(int i = 0 ; i < m ; ++i)
     cin >> b[i];
+    
+    //gasirea secventei
     for(int i = 0 ; i <= n-m ; ++i)
     {
         int cnt=0;
@@ -24,6 +27,8 @@ int main()
         ok++;
         }
     }
+    
+    //afisarea rezulatului:
     if(ok==0)
     cout << "NU";
     else
