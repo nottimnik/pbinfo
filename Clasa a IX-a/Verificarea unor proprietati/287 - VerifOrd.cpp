52f763b967584dd9d58a354ecdc38004265ecf04
Timnik <72356939+Timnikk@users.noreply.github.com>
2021-01-06 13:51:40 -0800

    Create 287 - VerifOrd.cpp

---
+++ Clasa a IX-a/Verificarea unor proprietati/287 - VerifOrd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[501];
+    int rez[11];
+    for(int i = 1; i <= n; ++i){
+        int nelem;
+        cin >> nelem;
+        for(int j = 1; j <= nelem; ++j)
+            cin >> a[j];
+        bool crescator = true;
+        for(int j = 2; j <= nelem; ++j)
+            if(a[j] < a[j-1])
+                crescator = false;
+        if(crescator)
+            rez[i] = 1;
+        else
+            rez[i] = 0;
+    }
+
+    for(int i = 1; i <= n; ++i)
+        cout << rez[i] << ' ';
+
+    return 0;
+}
