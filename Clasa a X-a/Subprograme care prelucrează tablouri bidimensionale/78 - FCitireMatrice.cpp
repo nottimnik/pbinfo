7cea8dece8103854130da25c69f02de62efb88f3
Timnik <72356939+Timnikk@users.noreply.github.com>
2021-01-20 10:52:43 -0800

    Create 78 - FCitireMatrice.cpp

---
+++ Clasa a X-a/Subprograme care prelucrează tablouri bidimensionale/78 - FCitireMatrice.cpp
@@ -0,0 +1,8 @@
+void citmat(int a[][100], int& n, int& m){
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            cin >> a[i][j];
+        }
+    }
+}
