6795b06cd9a843c4bb016eda5afe79fd536e73dd
timnik <72356939+nottimnik@users.noreply.github.com>
2021-02-05 07:14:39 -0800

    Create 122 - CifreBinare.cpp

---
+++ Clasa a IX-a/Baze de numeratie/122 - CifreBinare.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int cn = n, cm = m;
+    int a = 0, b = 0;
+    while(n) {
+        if(n%2==1) {
+            ++a;
+        }
+        n /= 2;
+    }
+    while(m) {
+        if(m%2==1) {
+            ++b;
+        }
+        m /= 2;
+    }
+    
+    if(a > b) {
+        cout << cn;
+        return 0;
+    }
+
+    cout << cm;
+
+    return 0;
+}
