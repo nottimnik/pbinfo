e750c26f5dc7daece6e3f008a369fbdef91dbd79
timnik <72356939+nottimnik@users.noreply.github.com>
2023-10-30 17:23:50 -0400

    Create 4482 - PrefixSufixMax1.cpp

---
+++ Clasa a X-a/Funcții predefinite cu șiruri de caractere/4482 - PrefixSufixMax1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+bool palindrom(char a[]) {
+    
+    int i = 0, j = strlen(a) - 1;
+    
+    while(i < j) {
+        if(a[i] != a[j]) return false;
+        i++;j--;
+        
+    }
+    
+    return true;
+    
+}
+
+int main() {
+    char a[1001]; cin.getline(a, 1001);
+    
+    char ca[1001];
+    strcpy(ca, a);
+    
+    char suf[1001], pre[1001];
+    
+    char aux[1001] = { NULL };
+    
+    for(int i = 0;a[i];i++) {
+        
+        aux[i] = a[i];
+        
+        if(palindrom(aux)) {
+            strcpy(suf, aux);
+        }
+        
+    }
+    
+    int lun = strlen(a) - 1;
+    
+    char aux2[1001] = { NULL };
+    
+    int ind = 0;
+    
+    for(int i = lun;a[i];i--) {
+        
+        aux2[ind] = a[i];
+        ind++;
+        
+        if(palindrom(aux2)) {
+            strcpy(pre, aux2);
+        }
+        
+    }
+    
+    
+    
+    
+    cout << suf << ' ' << pre;
+    
+
+    return 0;
+}
