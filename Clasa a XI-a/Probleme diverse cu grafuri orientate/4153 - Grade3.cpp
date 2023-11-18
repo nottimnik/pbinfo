f4ada6ea37c33c26faf74f965d740b8d5b3f8235
timnik <72356939+nottimnik@users.noreply.github.com>
2023-11-18 08:07:30 -0500

    Create 4153 - Grade3.cpp

---
+++ Clasa a XI-a/Probleme diverse cu grafuri orientate/4153 - Grade3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int inter[100], extra[100];
+
+int main() {
+    int n, m; cin >> n >> m;
+    int a, b;
+    for(int i = 1;i<=m;i++) {
+        cin >> a >> b;
+        extra[a]++;
+        inter[b]++;
+    }
+    
+    int nr = 0;
+    
+    for(int i = 1;i<=n;i++) 
+        if(extra[i] < inter[i]) nr++;
+    
+    
+    cout << nr << endl;
+    
+    for(int i = 1;i<=n;i++)
+        if(extra[i] < inter[i]) cout << i << ' ';
+
+   
+    
+    return 0;
+}
